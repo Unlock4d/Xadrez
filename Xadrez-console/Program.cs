c8186b802b892e4ffcdcba6bf61fa6e405099773
Frederico <frederico.fernandes2001@gmail.com>
2022-03-16 15:52:53 +0000

    Peças brancas e pretas postas

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,17 +8,11 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            
+
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(4, 5));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 3));
-                Tela.imprimirTabuleiro(tab);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                Tela.imprimirTabuleiro(partida.tab);
                 Console.WriteLine();
             }
             catch (TabuleiroException e)
--- Xadrez-console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -27,23 +27,35 @@ namespace tabuleiro
         public bool existePeca(Posicao pos)
         {
             validarPosicao(pos);
-            return peca(pos) != null;   
+            return peca(pos) != null;
         }
-        public void colocarPeca(Peca p,Posicao pos)
+        public void colocarPeca(Peca p, Posicao pos)
         {
             if (existePeca(pos))
             {
                 throw new TabuleiroException("Já existe uma peça nessa posição!");
             }
-            pecas[pos.linha, pos.coluna] = p;    
+            pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux; 
         }
 
 
 
         public bool posicaoValida(Posicao pos)
         {
-            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
             {
                 return false;
             }
