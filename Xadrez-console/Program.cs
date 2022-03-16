ce46e8487dedf0ceddba1ed962e87c77050c9caf
Frederico <frederico.fernandes2001@gmail.com>
2022-03-16 14:24:36 +0000

    Adicionei Tabuleiro Peca e Cor

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
+
 namespace Xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: "+P);
-            Console.ReadLine();
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine();
         }
     }
 }
