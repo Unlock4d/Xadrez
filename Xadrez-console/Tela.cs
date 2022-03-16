71c616e36ce2fe9ef05ba1d1a32de610ef784257
Frederico <frederico.fernandes2001@gmail.com>
2022-03-16 16:26:12 +0000

    Movimentação peças

--- Xadrez-console/Tela.cs
+++ Xadrez-console/Tela.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using tabuleiro;
+using xadrez;
 
 namespace Xadrez_console
 {
@@ -31,9 +32,17 @@ namespace Xadrez_console
             Console.WriteLine("  a b c d e f g h");
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca)
         {
-            if(peca.cor == Cor.Branca)
+            if (peca.cor == Cor.Branca)
             {
                 Console.Write(peca);
             }
