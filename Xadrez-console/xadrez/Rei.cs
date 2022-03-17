fd9aedb3592aa1005bc2256d77b88128fc0f9089
Frederico <frederico.fernandes2001@gmail.com>
2022-03-17 16:20:46 +0000

    XEQUEMATE

--- Xadrez-console/xadrez/Rei.cs
+++ Xadrez-console/xadrez/Rei.cs
@@ -41,6 +41,11 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
             pos.definirValores(posicao.linha + 1, posicao.coluna);
             if (tab.posicaoValida(pos) && podeMover(pos))
             {
